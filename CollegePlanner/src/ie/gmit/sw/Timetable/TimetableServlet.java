708a79743522704ed1032a0b687cccefeb387f17
Gareth Lysnkey <lynskey123@gmail.com>
2017-02-03 19:56:12 +0000

    added doPost method
    takes the input from the form and perform your actions

--- CollegePlanner/src/ie/gmit/sw/Timetable/TimetableServlet.java
+++ CollegePlanner/src/ie/gmit/sw/Timetable/TimetableServlet.java
@@ -1,8 +1,26 @@
 package ie.gmit.sw.Timetable;
 
+import java.io.IOException;
+
 import javax.servlet.Servlet;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class TimetableServlet extends HttpServlet implements Servlet {
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(request, response);
+		String[] days = request.getParameterValues("day");
+		String title = request.getParameter("title");
+		int starttime = Integer.parseInt(request.getParameter("starttime"));
+		int endtime = Integer.parseInt(request.getParameter("endtime"));
+		
+	}
+	
+	
 
 }
