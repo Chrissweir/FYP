c25a5aad12a0eb57e5a6057dcc85fbf48e329181
paulyd123 <g00297086@gmit.ie>
2017-02-06 16:33:33 +0000

    Updating java todo class
    Created setters and getters

--- CollegePlanner/src/ie/gmit/sw/Todo/ToDoClass.java
+++ CollegePlanner/src/ie/gmit/sw/Todo/ToDoClass.java
@@ -1,5 +1,39 @@
 package ie.gmit.sw.Todo;
 
 public class ToDoClass {
+	
+	private int id;
+	private String name;
+	private String description;
+	private boolean complete = false;
+	
+	
+	public void setId(int id) {
+	  this.id = id;
+	}
+	
+	public String getName() {
+	  return name;
+	}
+	
+	public void setName(String name) {
+	  this.name = name;
+	}
+	
+	public String getDescription() {
+	  return description;
+	}
+	
+	public void setDescription(String description) {
+	  this.description = description;
+	}
+	
+	public boolean isComplete() {
+	  return complete;
+	}
+	
+	public void setComplete(boolean complete) {
+	  this.complete = complete;
+	}
 
 }
