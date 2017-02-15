d36720033a5dafdaa0724c38a3a54bb7e2f75a05
Christopher Weir <christopher.weir@hotmail.com>
2017-02-15 18:03:01 +0000

    Updates

--- CollegePlanner/src/ie/gmit/sw/Profile/Profile.java
+++ CollegePlanner/src/ie/gmit/sw/Profile/Profile.java
@@ -216,4 +216,4 @@ public class Profile extends HttpServlet {
 		String ConnectionString ="jdbc:postgresql://ec2-54-75-239-190.eu-west-1.compute.amazonaws.com:5432/dc6f77btle9oe3?user=dmbleakzbhlbnl&password=b08ab093aa5b03c4047c541ceab2b23daa4fb5198e48d56f804319695455d754&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
 		return DriverManager.getConnection(ConnectionString);
 	}
-}
+}//219
