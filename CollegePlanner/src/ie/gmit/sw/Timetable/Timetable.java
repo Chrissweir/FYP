3aead0db6965fe2f21b306d89ab91d18a4879eb9
Gareth Lysnkey <lynskey123@gmail.com>
2017-02-16 18:26:16 +0000

    Added JavaDocs for Timetable.java

--- CollegePlanner/src/ie/gmit/sw/Timetable/Timetable.java
+++ CollegePlanner/src/ie/gmit/sw/Timetable/Timetable.java
@@ -8,11 +8,20 @@ public class Timetable {
 	//private list that takes in modules
 	private List<Module> classes = new ArrayList<Module>();
 
-	//Getter for module list in order to access the lists object information
+	
+	/**
+	 * Getter for module list in order to access the lists object information
+	 * @return
+	 */
 	public List<Module> getClasses() {
 		return classes;
 	}
 	
+	
+	/**
+	 * Method for adding new classes
+	 * @param module
+	 */
 	public void addClass(Module module)
 	{
 		classes.add(module);
