a35864d7720d865c12a8a6157a278f2c0b5b3432
Gareth Lysnkey <lynskey123@gmail.com>
2017-02-16 18:22:03 +0000

    fix code and commented it

--- CollegePlanner/src/ie/gmit/sw/Timetable/Timetable.java
+++ CollegePlanner/src/ie/gmit/sw/Timetable/Timetable.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 public class Timetable {
 	
+	//private list that takes in modules
 	private List<Module> classes = new ArrayList<Module>();
 
-	public List getClasses() {
+	//Getter for module list in order to access the lists object information
+	public List<Module> getClasses() {
 		return classes;
 	}
 	
