9d080e3c50f088402d0b5c808e013e41e78f9bb8
Christopher Weir <christopher.weir@hotmail.com>
2017-04-04 14:42:00 +0100

    Changes

--- CollegePlanner/src/ie/gmit/sw/Error/ErrorHandler.java
+++ CollegePlanner/src/ie/gmit/sw/Error/ErrorHandler.java
@@ -1,24 +1,34 @@
 package ie.gmit.sw.Error;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * @author Christopher Weir - G00309429	
+ * 
+ * Handles the errors caught in try/catch statements
+ *
+ */
 public class ErrorHandler extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher("ErrorHandler.jsp");
 		rd.forward(request, response);
 	}
 
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher("ErrorHandler.jsp");
